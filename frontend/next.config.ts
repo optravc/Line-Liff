985c4136d7dc3087378c1d591f4b1045cf55fab8
optra <noptrapk@gmail.com>
2026-07-14 08:35:24 +0700

    fix config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com", "profile.line-scdn.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "profile.line-scdn.net",
+      },
+    ],
   },
-  allowedDevOrigins: ["overstiff-patience-unmeticulously.ngrok-free.dev"],
 };
 
 export default nextConfig;
