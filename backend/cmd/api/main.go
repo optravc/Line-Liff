eecf2de750ce4df90aeb36e3d1bebb442ae44904
optravc <noptrapk@gmail.com>
2026-07-14 11:01:48 +0700

    add gin-contrib/cors

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,12 +4,21 @@ import (
 	"backend/internal/database"
 	"backend/internal/routes"
 
+	"github.com/gin-contrib/cors" 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.InitDB()
 	r := gin.Default()
+
+	// ตั้งค่า CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000", "https://line-liff-2p7b.vercel.app/"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	}))
+
 	routes.SetupBookingRoutes(r)
 	r.Run(":8080")
 }
