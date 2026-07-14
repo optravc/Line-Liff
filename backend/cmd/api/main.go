1aeb963926c8e964d535c531602fcc2042cb9455
optra <noptrapk@gmail.com>
2026-07-14 07:29:15 +0700

    rollback

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,34 +3,13 @@ package main
 import (
 	"backend/internal/database"
 	"backend/internal/routes"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-// นี่คือ Handler สำหรับ Vercel
-func Handler(w http.ResponseWriter, r *http.Request) {
-	app := setupRouter()
-	app.ServeHTTP(w, r)
-}
-
-// นี่คือฟังก์ชันตั้งค่า Router เพื่อใช้ร่วมกัน
-func setupRouter() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
+func main() {
+	database.InitDB()
 	r := gin.Default()
 	routes.SetupBookingRoutes(r)
-	return r
-}
-
-func main() {
-	// ถ้ามี environment variable "VERCEL" แสดงว่ารันบน Vercel
-	if os.Getenv("VERCEL") == "1" {
-		return 
-	}
-
-	// ถ้าไม่ใช่อยู่บน Vercel ให้รันแบบปกติในเครื่อง
-	database.InitDB()
-	r := setupRouter()
 	r.Run(":8080")
 }
